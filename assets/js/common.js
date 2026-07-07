c61cfd8369421fd91ed72ca3b692156399d53e8c
sanmakol <onedayofu01@gmail.com>
2026-07-07 16:53:49 +0700

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,1 +1,19 @@
+const API = "https://i.sanmakol.com";
 
+const toast = document.getElementById("toast");
+
+function showToast(text){
+
+    if(!toast) return;
+
+    toast.innerHTML = text;
+
+    toast.classList.add("show");
+
+    setTimeout(()=>{
+
+        toast.classList.remove("show");
+
+    },2200);
+
+}
