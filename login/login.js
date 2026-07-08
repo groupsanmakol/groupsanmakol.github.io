c055ef1fcf39a0f4dc310d3a669a7ecee6ea06c1
sanmakol <onedayofu01@gmail.com>
2026-07-08 20:14:57 +0700

    Update login.js

--- login/login.js
+++ login/login.js
@@ -1,6 +1,94 @@
-localStorage.setItem(
-    "user",
-    JSON.stringify(json.user)
-);
+const API = "https://i.sanmakol.com";
 
-window.location.href="/";
+const loginBtn =
+document.getElementById("loginBtn");
+
+const error =
+document.getElementById("error");
+
+loginBtn.onclick = login;
+
+async function login(){
+
+    error.innerHTML = "";
+
+    const email =
+    document
+    .getElementById("email")
+    .value
+    .trim();
+
+    const password =
+    document
+    .getElementById("password")
+    .value
+    .trim();
+
+    if(!email || !password){
+
+        error.innerHTML =
+        "Vui lòng nhập đầy đủ thông tin.";
+
+        return;
+
+    }
+
+    loginBtn.disabled = true;
+
+    loginBtn.innerHTML =
+    "Đang đăng nhập...";
+
+    try{
+
+        const response =
+        await fetch(API + "/login",{
+
+            method:"POST",
+
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify({
+
+                email,
+                password
+
+            })
+
+        });
+
+        const json =
+        await response.json();
+
+        if(!json.success){
+
+            throw new Error(json.error);
+
+        }
+
+        localStorage.setItem(
+
+            "user",
+
+            JSON.stringify(json.user)
+
+        );
+
+        location.href="/";
+
+    }
+
+    catch(e){
+
+        error.innerHTML =
+        e.message;
+
+    }
+
+    loginBtn.disabled = false;
+
+    loginBtn.innerHTML =
+    "Đăng nhập";
+
+}
